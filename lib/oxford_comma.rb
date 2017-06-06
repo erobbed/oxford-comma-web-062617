020d606795fb0f769aff303c5f4d282820ba8396
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-06 15:05:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+    new_array = array.insert(-2, "and ")
+    new_array = "#{new_array[0...-1].join(", ")}" + "#{new_array.last}"
+  else
+    if array.length > 3
+      new_array = array.insert(-2, "and ")
+      new_array = "#{new_array[0...-1].join(", ")}" + "#{new_array.last}"
+    end
+  end
 end
